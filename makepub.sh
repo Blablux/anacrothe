c367a066ea32b052b4810dd6299f7146f2aaac1b
Blablux <hetrube@gmail.com>
2014-09-28 23:50:13 +0200

    First commit

---
+++ makepub.sh
@@ -0,0 +1,130 @@
+#! /bin/bash
+
+# automatic epub generation
+# needs unix programs :
+# - basename / dirname (to find the paths) (these programs are often pre-installed)
+# - sed (to make character replacement) (this program is often pre-installed)
+# - csplit (to split chapters) (this program is often pre-installed)
+# - recode (for html entities conversion)
+# - Perl (for markdown conversion)
+
+function senderror()
+{
+  echo "$1" ;
+  if [ -r $workingname.tmp ] ;
+  then
+    rm $workingname.tmp
+  fi
+  if [ -r $filepath/$filename.html ] ;
+  then
+    rm $filepath/$filename.html
+  fi
+  exit 1 ;
+}
+scriptpath=`dirname $0`
+
+# check if file exist
+if [ ! -z $1 ] ;
+then
+  filepath=`dirname $1`
+  filename=$(basename "$1")
+  ext="${filename##*.}"
+  filename="${filename%.*}"
+  workingname=$filepath/$filename
+  if [ ! -r $filepath/$filename.$ext ] ;
+  then
+    senderror "$workingname.$ext can't be read!"
+  fi
+else
+  senderror "You must supply the file to convert"
+fi
+
+# check if file is text and markdown
+if ! file --mime-type "$1" | grep -q text/plain$; then
+  echo "WARNING :$filename.$ext does not look like plain text"
+  echo -n "Do you want to continue [y/n] ? " ; read textfile
+  if [[ "$textfile" != [yY] ]] ; then
+    exit 1
+  elif [[ "$ext" != md && "$ext" != markdown && "$ext" != mdown && "$ext" != mkdn && "$ext" != mkd && "$ext" != mdwn && "$ext" != mdtxt && "$ext" != mdtext] ]] ;
+  then
+    echo "File doesn't use have a markdown extension."
+    echo -n "Do you wish to continue? [y/n] ?" ; read markdown
+    if [[ $markdown != [yY] ]] ;
+    then
+      exit 1
+    fi
+  fi
+fi
+
+# get metadata
+echo "Please provide the metadata"
+echo -n "Author: " ; read author
+echo -n "Title: " ; read title
+echo -n "Publisher: " ; read publisher
+echo "Description will be taken from data/description.txt"
+
+# apply language rules
+echo -n "Choose language [en/fr]: " ; read lang
+case $lang in
+  en|fr) sed -f sed_$lang.txt $1 > $workingname.tmp ;;
+  *) senderror "Unsupported language"
+esac
+
+# force line break when you forget them
+sed -i ':a;N;$!ba;s/\n/  \n/g' $workingname.tmp
+sed -i ':a;N;$!ba;s/  \n  \n/\n\n/g' $workingname.tmp
+
+# encode html chars, except for html special chars
+recode -dt u8..html $workingname.tmp
+
+# markdown conversion
+# cat $scriptpath/data/header.txt > $filepath/$filename.html
+perl $scriptpath/markdown.pl --html4tags $workingname.tmp > $workingname.html
+# echo "</body>" >> $filepath/$filename.html
+
+# replace <hr> with a html entity
+echo "Do you want to replace horizontal rules with" ;
+echo -n "a single html entity? [y/n] " ; read replacehr
+if [[ "$replacehr" == [yY] ]] ;
+then
+  echo -n "Choose the html entity to use: " ; read hr
+  case ${#hr} in
+  0)
+    senderror "No html entity was provided"
+  ;;
+  1)
+    hr=$(echo "$hr" | recode u8..html)
+    if [ ${#hr} = 1 ]
+    then
+      senderror "$hr is not a valid html entity"
+    fi
+    hr="\\$hr"
+    sed -i "s/<hr>/\n<p class=\"hr\">$hr<\/p>\n/g" $workingname.html
+  ;;
+  *)
+    if [[ ! $hr =~ ^\& ]];
+    then
+      hr="&$hr"
+    fi
+    if [[ ${hr: -1} != ";" ]];
+    then
+      hr="$hr;"
+    fi
+    hrcheck=$(echo "$hr" | recode html..u8)
+    if [ ${#hrcheck} != 1 ]
+    then
+      senderror "$hr is not a valid html entity"
+    fi
+    hr="\\$hr"
+    sed -i "s/<hr>/\n<p class=\"hr\">$hr<\/p>\n/g" $workingname.html
+  ;;
+  esac
+fi
+
+# spliting chapters
+rm $workingname.tmp
+mkdir $filepath/Text
+csplit -sz -f "$filepath/Text/part" $workingname.html '/^<h1>/' {*}
+rm $workingname.html
+
+
